aa37267aa248bcd367d509599b24bc1d134c882d
patidar-mayank <210305124102@paruluniversity.ac.in>
2024-07-04 17:50:11 +0530

    update index page

--- mysite/myapps/forms.py
+++ mysite/myapps/forms.py
@@ -1,7 +1,8 @@
+
 from django.forms import ModelForm
 from .models import Expense
+
 class ExpenseForm(ModelForm):
     class Meta:
-        model=Expense
-        fields={'name','amount','category'}
-        
+        model = Expense
+        fields = ('name','amount','category')
--- mysite/myapps/views.py
+++ mysite/myapps/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from .forms import ExpenseForm
+from .models import Expense
 # Create your views here.
 def index(request):
+    if request.method =="POST":
+        expense = ExpenseForm(request.POST)
+        if expense.is_valid():
+            expense.save()
+
+    expenses=Expense.objects.all()
     expense_form=ExpenseForm()
 
-    return render(request,'myapp/index.html',{'expense_form':expense_form})
+    return render(request,'myapp/index.html',{'expense_form':expense_form,'expenses':expenses})
