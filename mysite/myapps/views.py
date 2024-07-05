26f8004fd0bc4633f49d53db42a9d7fb67304d2a
patidar-mayank <210305124102@paruluniversity.ac.in>
2024-07-05 06:23:57 +0530

    update index page

--- mysite/myapps/views.py
+++ mysite/myapps/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .forms import ExpenseForm
 from .models import Expense
 # Create your views here.
@@ -12,3 +12,21 @@ def index(request):
     expense_form=ExpenseForm()
 
     return render(request,'myapp/index.html',{'expense_form':expense_form,'expenses':expenses})
+
+def edit(request,id):
+    expense = Expense.objects.get(id=id)
+    expense_form = ExpenseForm(instance=expense)
+    if request.method =="POST":
+        expense = Expense.objects.get(id=id)
+        form = ExpenseForm(request.POST,instance=expense)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+        
+    return render(request,'myapp/edit.html',{'expense_form':expense_form})
+
+def delete(request,id):
+    if request.method =='POST' and 'delete' in request.POST:
+        expense = Expense.objects.get(id=id)
+        expense.delete()
+    return redirect('index')
