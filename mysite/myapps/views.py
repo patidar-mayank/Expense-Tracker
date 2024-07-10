bd401c3ff15c5f3147aa7255beca9b9f2869a969
patidar-mayank <210305124102@paruluniversity.ac.in>
2024-07-10 11:44:18 +0530

    updated views.py

--- mysite/myapps/views.py
+++ mysite/myapps/views.py
@@ -1,17 +1,44 @@
 from django.shortcuts import render,redirect
 from .forms import ExpenseForm
 from .models import Expense
+import datetime
+from django.db.models import Sum
 # Create your views here.
 def index(request):
     if request.method =="POST":
         expense = ExpenseForm(request.POST)
         if expense.is_valid():
             expense.save()
+            
+    expenses = Expense.objects.all()
+    total_expenses = expenses.aggregate(Sum('amount'))
+    
+    
+    
+    #Logic to calculate 365 days expenses
+    last_year = datetime.date.today() - datetime.timedelta(days=365)
+    data = Expense.objects.filter(date__gt=last_year)
+    yearly_sum = data.aggregate(Sum('amount'))
+    
+    #Logic to calculate 30 days expenses
+    last_month = datetime.date.today() - datetime.timedelta(days=30)
+    data = Expense.objects.filter(date__gt=last_month)
+    monthly_sum = data.aggregate(Sum('amount'))
+    
+     #Logic to calculate 7 days expenses
+    last_week = datetime.date.today() - datetime.timedelta(days=7)
+    data = Expense.objects.filter(date__gt=last_week)
+    weekly_sum = data.aggregate(Sum('amount'))
+    
+    daily_sums = Expense.objects.filter().values('date').order_by('date').annotate(sum=Sum('amount'))
+    
+    
+    categorical_sums = Expense.objects.filter().values('category').order_by('category').annotate(sum=Sum('amount'))
+    print(categorical_sums)
+    
+    expense_form = ExpenseForm()
+    return render(request,'myapp/index.html',{'expense_form':expense_form,'expenses':expenses,'total_expenses':total_expenses,'yearly_sum':yearly_sum,'weekly_sum':weekly_sum,'monthly_sum':monthly_sum,'daily_sums':daily_sums,'categorical_sums':categorical_sums})
 
-    expenses=Expense.objects.all()
-    expense_form=ExpenseForm()
-
-    return render(request,'myapp/index.html',{'expense_form':expense_form,'expenses':expenses})
 
 def edit(request,id):
     expense = Expense.objects.get(id=id)
